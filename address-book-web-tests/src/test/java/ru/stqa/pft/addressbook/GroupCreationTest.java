7a7dcd27fd4b7030a296f17979855d0fd766e6e8
Dmitriy Afanasev <d.afanasev@tinkoff.ru>
2021-01-05 19:59:06 +0500

    Login moved to @Before
    Login moved to @Before

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -5,31 +5,35 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class GroupCreationTest {
-  private WebDriver driver;
+   FirefoxDriver wd;
+
   @BeforeClass
-  public void setUp() {
-    driver = new FirefoxDriver();
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.cssSelector("input:nth-child(7)")).click();
   }
 
   @Test
-  public void groupCreation() {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).sendKeys("666");
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).sendKeys("777");
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).sendKeys("888");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
+  public void testGroupCreation() {
+    wd.findElement(By.linkText("groups")).click();
+    wd.findElement(By.name("new")).click();
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).sendKeys("666");
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).sendKeys("777");
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).sendKeys("888");
+    wd.findElement(By.name("submit")).click();
+    wd.findElement(By.linkText("group page")).click();
   }
 }
