6ce6fc0caa1a924acf5d8685c4df4c84ac7f9a5a
Dmitriy Afanasev <d.afanasev@tinkoff.ru>
2021-01-06 08:50:01 +0500

    Before split ApplicationManager
    Before split ApplicationManager

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/AplicationManager.java
@@ -6,8 +6,7 @@ import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class AplicationManager {
-  FirefoxDriver wd;
+public class AplicationManager extends GroupHelper {
 
   public void init() {
     wd = new FirefoxDriver();
@@ -28,36 +27,8 @@ public class AplicationManager {
     wd.findElement(By.cssSelector("input:nth-child(7)")).click();
   }
 
-  public void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
-  }
-
   public void goToGroupPage() {
     wd.findElement(By.linkText("groups")).click();
   }
 
-  public void deleteGroup() {
-    wd.findElement(By.name("delete")).click();
-  }
-
-  public void selectGroups() {
-    wd.findElement(By.name("selected[]")).click();
-  }
 }
