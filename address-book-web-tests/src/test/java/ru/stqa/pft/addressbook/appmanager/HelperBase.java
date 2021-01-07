00829ee4872736ec44f934f79694fcfbfb87809e
Dmitriy Afanasev <d.afanasev@tinkoff.ru>
2021-01-07 19:12:33 +0500

    Added check for field text
    if a field has suitable data dont dill the field with new data

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -21,6 +21,8 @@ public class HelperBase {
   protected void type(By locator, String text) {
     click(locator);
     if (text != null) { //Не заполнять в полях значения по умолчанию
+      String existingText = wd.findElement(locator).getAttribute("value"); //Получить значение из поля ввода
+      if (! text.equals(existingText)) // Не заполнять поле новым значением если в поле уже введено нужное нам значение
       wd.findElement(locator).clear();
       wd.findElement(locator).sendKeys(text);
     }
